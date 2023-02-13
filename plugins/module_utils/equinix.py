0ac7cb19e78a15eed59c03274e179ac978e2a338
Tomas Karasek <tom.to.the.k@gmail.com>
2023-02-13 17:21:17 +0100

    fixes in source code file form

--- plugins/module_utils/equinix.py
+++ plugins/module_utils/equinix.py
@@ -1,6 +1,9 @@
-# (c) 2023, Equnix DevRel Team (@equinix)
-#
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt) 
+
+
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
--- plugins/module_utils/metal_api.py
+++ plugins/module_utils/metal_api.py
@@ -1,5 +1,6 @@
-# (c) 2023, Tomas Karasek (@t0mk) <tom.to.the.k@gmail.com>
-#
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
