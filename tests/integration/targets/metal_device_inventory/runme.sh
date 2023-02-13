751a7d5f2036d2e55a033c4dde153f0f6a7ba907
Tomas Karasek <tom.to.the.k@gmail.com>
2023-02-13 10:21:52 +0100

    add test for metal_device inventrory

--- tests/integration/targets/metal_device_inventory/runme.sh
+++ tests/integration/targets/metal_device_inventory/runme.sh
@@ -2,20 +2,16 @@
 
 set -eux
 
-# Create testing instance
-ansible-playbook playbooks/setup_instance.yml "$@"
+# Create testing device
+ansible-playbook playbooks/setup_metal_device.yml "$@"
 
 # Test an inventory with no filter
-ansible-playbook playbooks/create_inventory.yml --extra-vars "template=nofilter.instance.yml" "$@"
-ANSIBLE_INVENTORY=nofilter.instance.yml ansible-playbook playbooks/test_inventory_nofilter.yml "$@"
-
-# Test an inventory with a filter
-ansible-playbook playbooks/create_inventory.yml --extra-vars "template=filter.instance.yml" "$@"
-ANSIBLE_INVENTORY=filter.instance.yml ansible-playbook playbooks/test_inventory_filter.yml "$@"
+ansible-playbook playbooks/create_inventory.yml --extra-vars "template=nofilter.metal_device.yml" "$@"
+ANSIBLE_INVENTORY=nofilter.metal_device.yml ansible-playbook playbooks/test_inventory_nofilter.yml "$@"
 
 # Test an inventory with keyed groups filter
-ansible-playbook playbooks/create_inventory.yml --extra-vars "template=keyedgroups.instance.yml" "$@"
-ANSIBLE_INVENTORY=keyedgroups.instance.yml ansible-playbook playbooks/test_inventory_keyedgroups.yml "$@"
+ansible-playbook playbooks/create_inventory.yml --extra-vars "template=keyedgroups.metal_device.yml" "$@"
+ANSIBLE_INVENTORY=keyedgroups.metal_device.yml ansible-playbook playbooks/test_inventory_keyedgroups.yml "$@"
 
 # Clean up
 ansible-playbook playbooks/teardown.yml "$@"
