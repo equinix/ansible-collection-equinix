8d9b81c8577f78416ef092fc0dd6f71f16b438d0
Antonin Rykalsky <antonin@celaeno.cz>
2023-11-29 10:24:21 +0100

    feat: metal vrf info

--- plugins/modules/metal_vrf_info.py
+++ plugins/modules/metal_vrf_info.py
@@ -8,7 +8,7 @@
 
 DOCUMENTATION = '''
 author: Equinix DevRel Team (@equinix) <support@equinix.com>
-description: Gather information about Equinix Metal resources
+description: Gather information about Equinix VRFs
 module: metal_vrf_info
 notes: []
 options:
@@ -20,26 +20,43 @@ options:
   project_id:
     description:
     - Project ID where the VRF will be deployed.
+    required: true
+    type: str
+  vrf_id:
+    description:
+    - ID of the VRF resource
     required: false
     type: str
 requirements: null
-short_description: Gather information about Equinix Metal resources
+short_description: Gather VRFs
 '''
 EXAMPLES = '''
-- name: Gather information about all resources in parent resource
+- name: Gather VRF information for a specific project
   hosts: localhost
   tasks:
   - equinix.cloud.metal_vrf_info:
       project_id: 2a5122b9-c323-4d5c-b53c-9ad3f54273e7
+- name: Retrieve information about resources within a specific VRF
+  hosts: localhost
+  tasks:
+  - equinix.cloud.metal_vrf_info:
+      project_id: 2a5122b9-c323-4d5c-b53c-9ad3f54273e7
+      vrf_id: f421024d-c1e6-4886-a64d-5b2515696200
 '''
 RETURN = '''
 resources:
   description: Found resources
   returned: always
   sample:
-  - "\n\n[  \n  {\n    \"id\": \"31d3ae8b-bd5a-41f3-a420-055211345cc7\",\n    \"name\"\
-    : \"ansible-integration-test-resource-csle6t2y-resource2\",\n    \"parent_resource_id\"\
-    : \"845b45a3-c565-47e5-b9b6-a86204a73d29\"\n  }\n]"
+  - "\n\n[\n        {\n            \"description\": \"Test VRF with ASN 65000\",\n\
+    \            \"id\": \"8b24de5b-c70e-4a4e-9dd2-064ceb09c587\",\n            \"\
+    ip_ranges\": [\n                \"192.168.100.0/25\",\n                \"192.168.200.0/25\"\
+    \n            ],\n            \"local_asn\": 65000,\n            \"metro\": {\n\
+    \                \"href\": \"/metal/v1/locations/metros/108b2cfb-246b-45e3-885a-bf3e82fce1a0\"\
+    ,\n                \"id\": \"108b2cfb-246b-45e3-885a-bf3e82fce1a0\"\n        \
+    \    },\n            \"name\": \"ansible-integration-test-vrf-nw6dgvh5\",\n  \
+    \          \"project_id\": \"06aea391-fd87-4cc7-9f4b-76f9e38fd4a4\"\n        }\n\
+    \    ]"
   type: dict
 '''
 
@@ -62,28 +79,49 @@ module_spec = dict(
     project_id=SpecField(
         type=FieldType.string,
         description=['Project ID where the VRF will be deployed.'],
+        required=True
+    ),
+    vrf_id=SpecField(
+        type=FieldType.string,
+        description=['ID of the VRF resource'],
     ),
 )
 
 specdoc_examples = ['''
-- name: Gather information about all resources in parent resource
+- name: Gather VRF information for a specific project
   hosts: localhost
   tasks:
       - equinix.cloud.metal_vrf_info:
           project_id: 2a5122b9-c323-4d5c-b53c-9ad3f54273e7
 ''', '''
+- name: Retrieve information about resources within a specific VRF
+  hosts: localhost
+  tasks:
+      - equinix.cloud.metal_vrf_info:
+          project_id: 2a5122b9-c323-4d5c-b53c-9ad3f54273e7
+          vrf_id: f421024d-c1e6-4886-a64d-5b2515696200
 ''',
                     ]
 
 result_sample = ['''
 
-[  
-  {
-    "id": "31d3ae8b-bd5a-41f3-a420-055211345cc7",
-    "name": "ansible-integration-test-resource-csle6t2y-resource2",
-    "parent_resource_id": "845b45a3-c565-47e5-b9b6-a86204a73d29"
-  }
-]''',
+[
+        {
+            "description": "Test VRF with ASN 65000",
+            "id": "8b24de5b-c70e-4a4e-9dd2-064ceb09c587",
+            "ip_ranges": [
+                "192.168.100.0/25",
+                "192.168.200.0/25"
+            ],
+            "local_asn": 65000,
+            "metro": {
+                "href": "/metal/v1/locations/metros/108b2cfb-246b-45e3-885a-bf3e82fce1a0",
+                "id": "108b2cfb-246b-45e3-885a-bf3e82fce1a0"
+            },
+            "name": "ansible-integration-test-vrf-nw6dgvh5",
+            "project_id": "06aea391-fd87-4cc7-9f4b-76f9e38fd4a4"
+        }
+    ]''',
 ]
 
 SPECDOC_META = getSpecDocMeta(
@@ -106,6 +144,8 @@ SPECDOC_META = getSpecDocMeta(
 def main():
     module = EquinixModule(
         argument_spec=SPECDOC_META.ansible_spec,
+        required_one_of=[('project_id', 'vrf_id')],
+        supports_check_mode=True,
         is_info=True,
     )
     try:
