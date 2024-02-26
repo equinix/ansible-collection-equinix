655db9a225699624fcd6bd5421949e2731c86e5a
Antonin Rykalsky <antonin@celaeno.cz>
2024-02-26 16:27:46 +0100

    test: dynamic inventory

--- tests/integration/targets/metal_device_inventory/runme.sh
+++ tests/integration/targets/metal_device_inventory/runme.sh
@@ -2,6 +2,11 @@
 
 set -eux
 
+cleanup() {
+    ansible-playbook playbooks/teardown.yml "$@"
+}
+trap cleanup EXIT
+
 # Create testing device
 ansible-playbook -vvv playbooks/setup_metal_device.yml "$@"
 
@@ -9,9 +14,10 @@ ansible-playbook -vvv playbooks/setup_metal_device.yml "$@"
 ansible-playbook playbooks/create_inventory.yml --extra-vars "template=nofilter.metal_device.yml" "$@"
 ANSIBLE_INVENTORY=nofilter.metal_device.yml ansible-playbook playbooks/test_inventory_nofilter.yml "$@"
 
+# Test an inventory with a filter
+ansible-playbook playbooks/create_inventory.yml --extra-vars "template=filter.metal_device.yml" "$@"
+ANSIBLE_INVENTORY=filter.metal_device.yml ansible-playbook playbooks/test_inventory_filter.yml "$@"
+
 # Test an inventory with keyed groups filter
 ansible-playbook playbooks/create_inventory.yml --extra-vars "template=keyedgroups.metal_device.yml" "$@"
 ANSIBLE_INVENTORY=keyedgroups.metal_device.yml ansible-playbook playbooks/test_inventory_keyedgroups.yml "$@"
-
-# Clean up
-ansible-playbook playbooks/teardown.yml "$@"
