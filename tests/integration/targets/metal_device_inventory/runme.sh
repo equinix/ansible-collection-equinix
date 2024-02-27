2bdcf8103cb6887765bb87b799ebe755f790f0bb
Antonin Rykalsky <antonin@celaeno.cz>
2024-02-27 15:38:16 +0100

    test: dynamic inventory

--- tests/integration/targets/metal_device_inventory/runme.sh
+++ tests/integration/targets/metal_device_inventory/runme.sh
@@ -16,6 +16,8 @@ ansible-playbook -vvv playbooks/setup_metal_device.yml --extra-vars "unique_id=$
 ansible-playbook playbooks/create_inventory.yml --extra-vars "template=nofilter.metal_device.yml" "$@"
 # Wait for file copy to complete. This prevents playbook errors in GitHub Actions tests.
 timeout 15 bash -c 'until [ -f "nofilter.metal_device.yml" ]; do sleep 1; done'
+cat nofilter.metal_device.yml || true
+ls -la .
 ANSIBLE_INVENTORY=nofilter.metal_device.yml ansible-playbook playbooks/test_inventory_nofilter.yml "$@"
 
 # Test an inventory with a filter
