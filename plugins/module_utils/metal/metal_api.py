935b00d085ec99250e92d5ebf95afaabbb13d59e
Bernardo Soares <Bernardo.Soares@eu.equinix.com>
2024-07-18 16:40:07 +0100

    metal_vlan_info: metro and facility as objects instead of strings
    Signed-off-by: Bernardo Soares <Bernardo.Soares@eu.equinix.com>

--- plugins/module_utils/metal/metal_api.py
+++ plugins/module_utils/metal/metal_api.py
@@ -231,8 +231,8 @@ METAL_METRO_RESPONSE_ATTRIBUTE_MAP = {
 VLAN_RESPONSE_ATTRIBUTE_MAP = {
     "id": "id",
     "description": optional_str('description'),
-    "metro": optional_str("metro"),
-    "facility": optional_str("facility"),
+    "metro": optional("metro"),
+    "facility": optional("facility"),
     "vxlan": "vxlan",
     "tags": "tags",
 }
